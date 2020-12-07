050e53fe96c6d237b41ca74856d23a3fe4481091
tjibbed <tjibbed@gmail.com>
2020-12-07 19:12:46 +0100

    Implementation of varying admission rate
    - Changes to output of admRateCalculator(): now also includes the underlying numbers
    -- output also includes moving averages
    -- allAvAR: mean over all previous data points
    -- movAvAR: moving average, center of 31 day interval. Days without observed/given numbers of occupied beds are linearly interpolated between given observations.
    
    - Implementation of varying admission rates (from confirmed case to hospital admission) in the bed forecast, wihtin hospital model.
    -- Code is currently hard-wired into a copied function, later this needs to be written as a general function again.

--- WithinHospModel.R
+++ WithinHospModel.R
@@ -326,12 +326,16 @@ getTimeLineFromCE<-function(ce,tlLength=NULL){
   return(timelineTemp)
 }
 
+
+
+################### Updated care path model, local data based ###################
+
 getLOSdistr<-function(untilDate,numPatients=10000){
   mypatsTemp<-createPatientsvLate(c(numPatients,0),
                                   0,
                                   (1-directICUProp),
                                   directICUProp,
-                                  c(1,0),
+                                  c(1,0),#pexp(1:100,1/3)-pexp(0:99,1/3),
                                   selectModelOnDate(UKFparaList,untilDate,"transfGW1"),
                                   selectModelOnDate(UKFparaList,untilDate,"transfICU"),
                                   selectModelOnDate(UKFparaList,untilDate,"disGW1"),
@@ -342,6 +346,24 @@ getLOSdistr<-function(untilDate,numPatients=10000){
   timelineTemp<-getTimeLineFromCE(rbind(mypatsTemp$hospChangeEvents,mypatsTemp$ICUChangeEvents))
   resLOSdistr<-timelineTemp$absolute/numPatients
   return(resLOSdistr)
+}
+
+
+extraInterPolate<-function(x){
+  lenX<-length(x)
+  #set firs and last element as first and last known (!is.na) element
+  if(is.na(x[1]))x[1]<-x[!is.na(x)][1]
+  if(is.na(x[lenX]))x[lenX]<-x[!is.na(x)][sum(!is.na(x))]
+  #Then, interpolate
+  unknowns<-(which(is.na(x)))
+  knowns<-(which(!is.na(x)))
+  begins<-unlist(lapply(unknowns,function(y)(max(knowns[knowns<y]))))
+  ends<-unlist(lapply(unknowns,function(y)(min(knowns[knowns>y]))))
+  distright<-ends-unknowns
+  distleft<-unknowns-begins
+  interVals<-((x[begins]*distright)+(x[ends]*distleft))/(distright+distleft)
+  x[unknowns]<-interVals
+  return(x)
 }
 
 admRateEstimator<-function(inciDF,bedObservations){
@@ -356,19 +378,142 @@ admRateEstimator<-function(inciDF,bedObservations){
   # - Hosp : All occupied beds in the hospital (GW+ICU)
   theDistr<-getLOSdistr(max(bedObservations$Date))
   # calculate the proportion admitted among all reported cases in the reversed LOS distribution for each "observed" day.
-  propAdmCalculator<-function(ddDate,ddHosp){
+  pressureCalculator<-function(ddDate){
     thisObsDate=which(inciDF$Date==ddDate)
     calcLen<-min(thisObsDate-1,length(theDistr))
     totalAdmPressure=sum(inciDF[((thisObsDate-calcLen)):(thisObsDate-1),"all"]*theDistr[calcLen:1])
-    return(ddHosp/totalAdmPressure)
+    return(totalAdmPressure)
   }
   #Begin calculating a week after the first case was reported.
-  inclBO<-bedObservations[(bedObservations$Date>(min(inciDF$Date)+6)),]
-  admProps<-unlist(lapply(1:nrow(inclBO),function(x)propAdmCalculator(inclBO[[x,"Date"]],inclBO[[x,"Hosp"]])))
+  inclRecs<-(inciDF$Date>(min(inciDF$Date)+6))
+  resDF<-data.frame(
+    Date=inciDF[inclRecs,"Date"],
+    admPress=unlist(lapply(inciDF[inclRecs,"Date"],function(x)pressureCalculator(x))),
+    cases=inciDF[inclRecs,"all"],
+    cLastweek=inciDF[inclRecs,"cLastweek"],
+    thisPop=inciDF[inclRecs,"thisPop"],
+    RKIcut=inciDF[inclRecs,"RKIcut"]
+  )
+
+  resDF=merge(resDF,bedObservations,by="Date",all=T)
+  resDF$admProps<-resDF$Hosp/resDF$admPress
+  resDF$allAvAR<-(unlist(lapply(resDF$Date,function(x)sum(resDF[resDF$Date<=x,"Hosp"],na.rm = T)/sum(resDF[resDF$Date<=x,"admPress"],na.rm = T))))
   
-  return(data.frame(Date=inclBO[,"Date"],
-                    admProps=admProps
-                      ))
+  calcThese<-resDF[!is.na(resDF$Hosp),]
+  resDF[!is.na(resDF$Hosp),"movAvAR"]<-(unlist(lapply(calcThese$Date,function(x)
+    sum(calcThese[(calcThese$Date<=(x+15))&(calcThese$Date>=(x-15)),"Hosp"],na.rm=T)/
+      sum(calcThese[(calcThese$Date<=(x+15))&(calcThese$Date>=(x-15)),"admPress"],na.rm = T)
+    )))
+  
+  resDF[is.na(resDF$Hosp),"movAvAR"]<-NA
+  resDF$movAvAR<-extraInterPolate(resDF$movAvAR)
+  
+  resDF$movAvWeightAR<-(unlist(lapply(resDF$Date,function(x)
+    sum(resDF[(resDF$Date<=(x+15))&(resDF$Date>=(x-15)),"Hosp"],na.rm=F)/
+      sum(resDF[(resDF$Date<=(x+15))&(resDF$Date>=(x-15)),"admPress"],na.rm = F)
+  )))
+  #NOTE: what to do with NAs?
+  
+  return(resDF)
+}
+
+createPatientsVaryAdmRate<-function(incidence,pLv,pN,pI,alphaH,alphaI,alphaD,deltaH,deltaI,deltaD,pHosp=NULL){
+  incidence<-round(incidence)
+  if((pN+pI)!=1) stop("Normal and ICU parts != 1")
+  pullPatients<-function(nPat,pL,day){
+    tAH<-sample(1:(length(alphaH)),nPat,prob = alphaH,replace = T)
+    tAI<-sample(1:(length(alphaI)),nPat,prob = alphaI,replace = T)
+    tAD<-sample(1:(length(alphaD)),nPat,prob = alphaD,replace = T)
+    tDH<-sample(1:(length(deltaH)),nPat,prob = deltaH,replace = T)
+    tDI<-sample(1:(length(deltaI)),nPat,prob = deltaI,replace = T)
+    tDD<-sample(1:(length(deltaD)),nPat,prob = deltaD,replace = T)
+    
+    timeDF<-data.frame(tAH,tAI,tAD,tDH,tDI,tDD,hospStay<-1,ICUstay<-1,stepStay<-1,type<-1)
+    
+    pType<-       # H I D H I D H I D 
+      sample(list(c(0,0,0,0,0,0,0,0,0,"L"),       #Stay at home (low-symptomatic)
+                  c(1,1,1,1,1,1,1,1,1,"H"),       #First to hospital
+                  c(1,0,1,0,1,1,1,1,1,"I")        #Immediate ICU admisison
+      ),
+      nPat,prob=c(pL, pN*(1-pL), pI*(1-pL)),replace = T
+      )
+    #hospChoice<-sample(1:length(pHosp),nPat,prob=pHosp,replace = T)
+    hospChoice<-rep(1,nPat)
+    
+    typeDF<-as.data.frame(t(matrix(unlist((pType)),nrow=10)))
+    typeDF2<-sapply(typeDF[,1:9],function(x)as.numeric(as.character(x)))
+    actualTimes<-(timeDF[,1:9]*typeDF2)
+    colnames(actualTimes)<-c("tAH","tAI","tAD","tDH","tDI","tDD","hospStay","ICUstay","stepStay")
+    actualTimes$type<-typeDF[,10]
+    actualTimes$hosp<-hospChoice
+    actualTimes$day<-day
+    return(actualTimes)
+  }
+  
+  allActualTimes<-Reduce(rbind,lapply(1:length(incidence),
+                                      function(x){
+                                        if(incidence[x]!=0)
+                                        {pullPatients(incidence[x],1-pLv[x],x)}
+                                        else {
+                                          data.frame(tAH=1,tAI=1,tAD=1,tDH=1,tDI=1,tDD=1,hospStay=1,ICUstay=1,stepStay=1,type="L",hosp=-1,day=x)
+                                        }
+                                      }
+  )
+  )
+  allActualTimes<-allActualTimes[allActualTimes$hosp!=-1,]
+  
+  hospPats<-allActualTimes[(allActualTimes$type=="H"),]
+  #End-of-stay is discharge from hospital:
+  hospPats$endStay<-hospPats$tDH
+  #Unless admission to ICu comes earlier:
+  hospPats[hospPats$tAI<hospPats$tDH,"endStay"]<-hospPats[hospPats$tAI<hospPats$tDH,"tAI"]
+  #admission is current day plus time to hospital admission:
+  hospadm<-hospPats[,"day"]+hospPats[,"tAH"]-1
+  hospdis<-hospadm+hospPats[,"endStay"]
+  
+  icuPats<-allActualTimes[(allActualTimes$type=="I")|
+                            ((allActualTimes$type=="H")&(allActualTimes$tAI<allActualTimes$tDH)),]
+  icuPats$endStay<-icuPats$tDI
+  icuPats[icuPats$tAD<icuPats$tDI,"endStay"]<-icuPats[icuPats$tAD<icuPats$tDI,"tAD"]
+
+  ICUadm<-icuPats[,"day"]+icuPats[,"tAH"]+icuPats[,"tAI"]-1
+  ICUdis<-ICUadm+icuPats[,"endStay"]
+  
+  stepdownPats<-allActualTimes[
+    ((allActualTimes$type=="I")&(allActualTimes$tAD<allActualTimes$tDI))|
+      ((allActualTimes$type=="H")&(allActualTimes$tAD<allActualTimes$tDI)&(allActualTimes$tAI<allActualTimes$tDH)),]
+  
+  stepDownAdm<-stepdownPats[,"day"]+stepdownPats[,"tAH"]+stepdownPats[,"tAI"]+stepdownPats[,"tAD"]-1
+  stepDownDis<-stepDownAdm+stepdownPats[,"tDD"]
+  
+  ICUChangeEvents<-data.frame(time=c(ICUadm,ICUdis),
+                              event=c(rep(1,length(ICUadm)),rep(-1,length(ICUdis))),
+                              hosp=c(icuPats$hosp,icuPats$hosp)
+  )
+  ICUChangeEvents<-ICUChangeEvents[order(ICUChangeEvents$time),]
+  HospChangeEvents<-data.frame(time=c(hospadm,hospdis),
+                               event=c(rep(1,length(hospadm)),rep(-1,length(hospdis))),
+                               hosp=c(hospPats$hosp,hospPats$hosp))
+  HospChangeEventsSD<-data.frame(time=c(stepDownAdm,stepDownDis),
+                                 event=c(rep(1,length(stepDownAdm)),rep(-1,length(stepDownDis))),
+                                 hosp=c(stepdownPats$hosp,stepdownPats$hosp))
+  HospChangeEvents<-rbind(HospChangeEvents,HospChangeEventsSD)
+  HospChangeEvents<-HospChangeEvents[order(HospChangeEvents$time),]
+  return(list(hospChangeEvents=HospChangeEvents,ICUChangeEvents=ICUChangeEvents))
+}
+
+withinHospitalMultiRunVaryAdmRate<-function(numRuns=10,incidenceRuns,admRateEsti,pN,pI,alphaH,alphaI,alphaD,deltaH,deltaI,deltaD,pHosp=NULL){
+
+  runNames=rep(unique(incidenceRuns$runNum),len=numRuns)
+  inHospRuns<-lapply(runNames,
+                     function(x){
+                       print(x)
+                       return(
+                         createPatientsVaryAdmRate(incidenceRuns[incidenceRuns$runNum==x,"underlying"],admRateEsti,pN,pI,alphaH,alphaI,alphaD,deltaH,deltaI,deltaD,pHosp=NULL)
+                       )
+                     }
+  )
+  return(inHospRuns)
 }
 
 getBedPrediction<-function(lx){
@@ -382,17 +527,17 @@ getBedPrediction<-function(lx){
   admHospPart<-(max(cumsum(getRawAdm(UKFparaList,startDhere)$cases))/selectedKreise[selectedKreise$Date==startDhere,"all"])
   
   directICUProp<-UKFparaList[[getDateIndex(UKFparaList,startDhere)]]$propDirectICU
-
+  
   inHRunWSD<-withinHospitalMultiRunvLate(numRuns=100,mrResList[[lx]],
-                                     (1-admHospPart),#pL proportion at home
-                                     (1-directICUProp)*admHospPart,#pN #Proportion to normal ward
-                                     (directICUProp)*admHospPart,#pI #Proportion direct to ICU
-                                     pexp(1:100,1/2)-pexp(0:99,1/2),
-                                     getAlphaIDistr(UKFparaList,startDhere),
-                                     getAlphaDDistr(UKFparaList,startDhere),
-                                     getDeltaHDistr(UKFparaList,startDhere),
-                                     getDeltaIDistr(UKFparaList,startDhere),
-                                     getDeltaDDistr(UKFparaList,startDhere)
+                                         (1-admHospPart),#pL proportion at home
+                                         (1-directICUProp)*admHospPart,#pN #Proportion to normal ward
+                                         (directICUProp)*admHospPart,#pI #Proportion direct to ICU
+                                         pexp(1:100,1/2)-pexp(0:99,1/2),
+                                         getAlphaIDistr(UKFparaList,startDhere),
+                                         getAlphaDDistr(UKFparaList,startDhere),
+                                         getDeltaHDistr(UKFparaList,startDhere),
+                                         getDeltaIDistr(UKFparaList,startDhere),
+                                         getDeltaDDistr(UKFparaList,startDhere)
   )
   return(list(
     rawResults=inHRunWSD
